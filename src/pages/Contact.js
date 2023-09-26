f4cd5666a203d6187695a433595a4b450a49d9d1
Musa Liman <musaliman27@gmail.com>
2023-09-26 01:15:19 +0100

    new update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,13 @@ import React from 'react';
 import WomanImg from '../img/contact/Image7.jpg';
 import { motion } from 'framer-motion';
 import { transition1 } from '../transitions';
+import { FiMail, FiPhone, FiInstagram } from 'react-icons/fi';
 
 const Contact = () => {
+  const emailLink = 'mailto:mua@gmail.com';
+  const whatsappLink = 'https://api.whatsapp.com/send?phone=123456789';
+  const instagramLink = 'https://instagram.com/artist';
+
   return (
     <motion.section
       initial={{ opacity: 0, y: '100%' }}
@@ -22,34 +27,45 @@ const Contact = () => {
             className='hidden lg:flex absolute bottom-0 left-0 right-0 top-72 -z-10'
           ></motion.div>
           <div className='lg:flex-1 lg:pt-32 px-4'>
-            <h1 className='h1'>Contact me</h1>
-            <p className='mb-8 lg:mx-6 mx-4 text-lg font-semibold text-primary'>
-              I would love to hear from you.
+            <h1 className='h1 lg:mx-4 mx-2'>Contact me</h1>
+            <p className='mb-8 lg:mx-6 mx-4 text-lg font-semibold text-black'>
+              I would love to bring your dream look to life, we can get started
+              , send an email, text on whatsapp or click my instagram profile
+              for more of my work.
             </p>
 
-            <form className='flex flex-col gap-y-4'>
-              <div className='flex gap-x-10'>
-                <input
-                  className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
-                  type='text'
-                  placeholder='Your name'
-                />
-
-                <input
-                  className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
-                  type='text'
-                  placeholder='Your email address'
-                />
+            <div className='flex flex-col gap-y-4 lg:mx-6 mx-4'>
+              <div className='flex items-center gap-2 text-wine'>
+                <FiMail />
+                <span>Gmail:</span>
+                <a href={emailLink} className='hover:text-black'>
+                  mua@gmail.com
+                </a>
               </div>
-              <input
-                className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
-                type='text'
-                placeholder='Your message'
-              />
-              <button className='btn mb-[30px] mx-auto lg:mx-0 self-start'>
-                Send it
-              </button>
-            </form>
+              <div className='flex items-center gap-2 text-wine'>
+                <FiPhone />
+                <span>WhatsApp number:</span>
+                <a
+                  href={whatsappLink}
+                  className='hover:text-black'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  123456789
+                </a>
+              </div>
+              <div className='flex items-center gap-2'>
+                <FiInstagram />
+                <a
+                  href={instagramLink}
+                  className='text-wine hover:text-black'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Instagram Profile
+                </a>
+              </div>
+            </div>
           </div>
           <motion.div
             initial={{ opacity: 0, y: '100%' }}
